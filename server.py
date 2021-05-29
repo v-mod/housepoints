e82704751b3899584be010ae3532173561eaf90c
v-mod <80989699+v-mod@users.noreply.github.com>
2021-05-29 20:04:38 +0100

    Update: 2.1

--- server.py
+++ server.py
@@ -2,20 +2,34 @@ from flask import Flask, render_template, request, redirect, url_for, session
 import UserManager as UM
 import os
 import housepointsManger
+import requests
 app = Flask(__name__)
 app.secret_key='AAAAB3NzaC1yc2EAAAABJQAAAQEAvI+0heuc2jKKSiaUEMTay7xsOhEOwapBsosHgo8jFbiELcXB1gwtELKmiLdkFRoowBb2Ga1VRJVtgeLtetM4FYu7xbRtoQB/E3tbnAJbiMy4pUCGMeI2lIFTFL0vWHGsqH/5qdoXu0dFijfdyxqvj/F5SZH7vpIXNZJu9Nvsr4UEnDWl16ndcVHsel1aMdW93I2OGLpEf8yvMR+Lq7ugVldUu2dC3FJMbZ4OkQiafDqA4ulLKk1SFRC0SsFlhIm/7XZVua4ckxEYdFRAn5NIC76ARyQUBANhIHhGkdApHm4m6ykhtozEPVagjIsNtuaZKFqOESL3ltIotHIHar/HL4Q'
 userManager = UM.UserManager()
 userInfo = None
 userName = None
+house1Name='York'
+house2Name='Tudor'
+house3Name='Lancaster'
+house4Name='Stuart'
+# Notes:
+#
+# House ids are...
+# House 1 is York
+# House 2 is Tudor
+# House 3 is Lancaster
+# House 4 is Stuart
+#
+# This is a Flask based web app
 @app.route('/')
 def homepage():
     winning='no one'
     chouse=housepointsManger.view()
-    york = chouse[0]
-    stuart = chouse[3]
-    lancaster = chouse[2]
-    tudor = chouse[1]
-    houseList = [{'house': 'York', 'points': int(york), 'color': 'yellow'}, {'house': 'Tudor', 'points': int(tudor), 'color': 'blue'}, {'house': 'Lancaster', 'points': int(lancaster), 'color': 'red'}, {'house': 'Stuart', 'points': int(stuart), 'color': '#00FF00'}]
+    house1 = chouse[0]
+    house4 = chouse[3]
+    house3 = chouse[2]
+    house2 = chouse[1]
+    houseList = [{'house': house1Name, 'points': int(house1), 'color': 'yellow'}, {'house': house2Name, 'points': int(house2), 'color': 'blue'}, {'house': house3Name, 'points': int(house3), 'color': 'red'}, {'house': house1Name, 'points': int(house4), 'color': '#00FF00'}]
     sortedHouseList = housepointsManger.sort(houseList)
     winner=sortedHouseList[0]
     if str(winner['points']) != '0':
@@ -56,7 +70,13 @@ def auth_service():
         return redirect(url_for('login'))
 @app.route('/HousePoints/Manage')
 def manage_housepoints():
-    userData = userManager.getDetails(session['UserId'])
+    pointsFile=open(r'House-Data/Points.txt','r')
+    points=pointsFile.read()
+    pointsFile.close()
+    try:
+        requests.get('https://westlodgeHPbackuprestore.azurewebsites.net/backup/8432658q647q259/'+points)
+    except:
+        print('INFO: WEB BACKUP ERR')
     chouse=housepointsManger.view()
     york = chouse[0]
     stuart = chouse[3]
@@ -65,18 +85,18 @@ def manage_housepoints():
     if session['role']=='teacher':
         return render_template('managehousepoints.html', york=york, lancaster=lancaster, tudor=tudor, stuart=stuart)
     elif session['role'] == 'admin' :
-        return render_template('remotecmd.html', userId=session['UserId'])
+        return render_template('remotecmd.html', userId=session['UserId'],york=york, lancaster=lancaster, tudor=tudor, stuart=stuart)
 @app.route('/service/housepoints/modify', methods=['POST'])
 def service_house_points_modify():
-    yorkP=request.form['yorkPlus']
-    yorkM=request.form['yorkMinus']
-    tudorP=request.form['tudorPlus']
-    tudorM=request.form['tudorMinus']
-    lancasterP=request.form['lancasterPlus']
-    lancasterM=request.form['lancasterMinus']
-    stuartP=request.form['stuartPlus']
-    staurtM=request.form['stuartMinus']
-    to_modify=yorkP,yorkM,tudorP, tudorM, lancasterP, lancasterM, stuartP, staurtM
+    house1P=request.form['yorkPlus']
+    house1M=request.form['yorkMinus']
+    house2P=request.form['tudorPlus']
+    house2M=request.form['tudorMinus']
+    house3P=request.form['lancasterPlus']
+    house3M=request.form['lancasterMinus']
+    house4P=request.form['stuartPlus']
+    house4M=request.form['stuartMinus']
+    to_modify=house1P,house1M,house2P, house2M, house3P, house3M, house4P, house4M
     housepointsManger.modify(to_modify)
     return redirect('/')
 @app.route('/auth/signup/<signup_key>')
@@ -105,11 +125,11 @@ def restore_housePoints():
 @app.route('/embed/housepoints')
 def house_embed():
     chouse=housepointsManger.view()
-    york = chouse[0]
-    stuart = chouse[3]
-    lancaster = chouse[2]
-    tudor = chouse[1]
-    houseList = [{'house': 'York', 'points': int(york), 'color': 'yellow'}, {'house': 'Tudor', 'points': int(tudor), 'color': 'blue'}, {'house': 'Lancaster', 'points': int(lancaster), 'color': 'red'}, {'house': 'Stuart', 'points': int(stuart), 'color': 'green'}]
+    house1 = chouse[0]
+    house4 = chouse[3]
+    house3 = chouse[2]
+    house2 = chouse[1]
+    houseList = [{'house': house1Name, 'points': int(house1), 'color': 'yellow'}, {'house': house2Name, 'points': int(house2), 'color': 'blue'}, {'house': house3Name, 'points': int(house3), 'color': 'red'}, {'house': house1Name, 'points': int(house4), 'color': '#00FF00'}]
     sortedHouseList = sortedHouseList = housepointsManger.sort(houseList)
     return render_template('embed_pointsView.html', houseList=sortedHouseList)
 @app.route('/service/auth/password',methods=['POST'])
@@ -140,5 +160,11 @@ def quick_login(userId, authKey):
 @app.route('/embed/calculator')
 def calculator():
     return render_template('calculator.html')
+@app.route('/services/restore/<points>')
+def restoreDataFromBR(points):
+    print('INFO: Sys engaging restore')
+    pointsFile=open(r'House-Data/Points.txt','w')
+    pointsFile.write(points)
+    pointsFile.close()
 if __name__ == '__main__':
     app.run(debug=True,  host='0.0.0.0')
